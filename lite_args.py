e1aa59fb956997a62bccc44f10eb39cb98516c80
Md Rezaul Karim <rkreza24@gmail.com>
2026-04-05 07:05:43 +0600

    garbage deletion

--- lite_args.py
+++
@@ -1,7 +0,0 @@
-import argparse
-import sys
-print(f"LITE_ARGS: sys.argv={sys.argv}")
-parser = argparse.ArgumentParser()
-parser.add_argument("--symbol", type=str)
-args = parser.parse_args()
-print(f"LITE_ARGS: SUCCESS. args.symbol={args.symbol}")
--- tmp/check_mt5.py
+++
@@ -1,8 +0,0 @@
-import MetaTrader5 as mt5
-
-if not mt5.initialize():
-    print(f"FAILED: MT5 initialize() failed, error code={mt5.last_error()}")
-    quit()
-
-print("SUCCESS: MT5 connected.")
-mt5.shutdown()
